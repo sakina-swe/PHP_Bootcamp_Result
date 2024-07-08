78352f7cbafea6fb677e58854a56a1fc1c77d04f
IsoqulovBekzod <146481317+IsoqulovBekzod@users.noreply.github.com>
2024-07-08 17:00:25 +0500

    Add files via upload

--- 3.php
+++ 3.php
@@ -1,57 +1,33 @@
 <?php
 
 declare(strict_types=1);
-date_default_timezone_set('Asia/Tashkent'); 
-$conn = new PDO(
-    'mysql:host=localhost;dbname=vaqt1',
-    'root',
-    'root'
-);
 
-?>
+class Currency
+{
+    const CB_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
+    private int $usdIndex = 0;
 
-<form action="3.php" method="post">
-    <label>
-        Arrived At
-        <input type="datetime-local" name="arrived_at" required>
-    </label><br>
-
-    <label>
-        Leaved At
-        <input type="datetime-local" name="leaved_at" required>
-    </label>
-    <button>Submit</button>
-</form>
-
-<?php
-
-if (!empty($_POST["arrived_at"]) && !empty($_POST["leaved_at"])) {
-    $arrived_at = (new DateTime($_POST['arrived_at']))->format('Y-m-d H:i:s');
-    $leaved_at  = (new DateTime($_POST['leaved_at']))->format('Y-m-d H:i:s');
-    
-    $sql = "INSERT INTO vaqt (arrived_at, leaved_at) VALUES (:arrived_at, :leaved_at)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':arrived_at', $arrived_at);
-    $stmt->bindParam(':leaved_at', $leaved_at);
-
-    if($stmt->execute()){
-        echo "Ma'lumotlar bazaga qo'shildi.";
-    }else{
-        echo "Ma'lumot bazaga qushilmadi.";
+    public function exchange(float|int $uzs, $currencyName): float|int
+    {
+        $valyuta = $this->customCurrencies()[$currencyName];
+        return $uzs / $valyuta;
     }
 
-    $sql = "SELECT * FROM vaqt";
-    $result = $conn->query($sql);
+    public function getCurrencyInfo()
+    {
+        $currencyInfo = file_get_contents(self::CB_URL);
+        return json_decode($currencyInfo);
+    }
 
-    if ($result->rowCount() > 0) {
-        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            echo '<h2>' ."ID : ". $row["id"] . '</h2>';
-            echo '<h2>' ."Arrived At : ". $row["arrived_at"] . '</h2>';
-            echo '<h2>' ."Leaved At : ". $row["leaved_at"] . '</h2>';
+    public function customCurrencies(): array
+    {
+        $currencies        = (array) $this->getCurrencyInfo();
+        $orderedCurrencies = [];
+        foreach ($currencies as $currency) {
+            
+            $orderedCurrencies[$currency->Ccy] = $currency->Rate;
         }
+
+        return $orderedCurrencies;
     }
 }
-else {
-    echo "Iltimos ma'lumotlarni kiriting.";
-}
-?>
